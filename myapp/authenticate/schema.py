a8a5ea20da69d768998e0a40b76689ab9c9537b1
shoumitro-cse <shoumitro26@gmail.com>
2021-10-16 10:07:11 +0600

    jwt auth added

--- myapp/authenticate/schema.py
+++ myapp/authenticate/schema.py
@@ -51,10 +51,20 @@ class CreateUserMutation(graphene.Mutation):
         return CreateUserMutation(user=user, token=token)
 
 
+class ObtainJSONWebToken(graphql_jwt.JSONWebTokenMutation):
+    user = graphene.Field(UserType)
+
+    @classmethod
+    def resolve(cls, root, info, **kwargs):
+        return cls(user=info.context.user)
+
+
 class Mutation(graphene.ObjectType):
     create_user = CreateUserMutation.Field()
 
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    # token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    token_auth = graphql_jwt.relay.ObtainJSONWebToken.Field()
+    # token_auth = ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
--- myapp/books_api/urls.py
+++ myapp/books_api/urls.py
@@ -18,14 +18,14 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 
 import books_api.schema
-# from api.schema import schema as api_schema
-# from authenticate.schema import schema as authenticate_schema
+from api.schema import schema as api_schema
+from authenticate.schema import schema as authenticate_schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('graphql', GraphQLView.as_view(graphiql=True)),
     path("graphql", GraphQLView.as_view(graphiql=True, schema=books_api.schema.schema)),
-    # path("book", GraphQLView.as_view(graphiql=True, schema=api_schema)),
-    # path("user", GraphQLView.as_view(graphiql=True, schema=authenticate_schema)),
+    path("book", GraphQLView.as_view(graphiql=True, schema=api_schema)),
+    path("user", GraphQLView.as_view(graphiql=True, schema=authenticate_schema)),
 
 ]
